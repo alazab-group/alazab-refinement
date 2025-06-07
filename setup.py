bb43982e0e0c055182697dc3ca0b01dfd0e1415f
Mohamed Azab <159525429+mohamedazab224@users.noreply.github.com>
2025-06-08 02:28:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
+# get version from __version__ variable in alazab_refinement/__init__.py
+from alazab_refinement import __version__ as version
+
 setup(
     name="alazab-refinement",
-    version="0.0.1",
-    description="Alazab Refinement App for Frappe",
-    author="Alazab Group",
+    version=version,
+    description="Al-Azab Construction Template Refinement for Frappe",
+    author="Al-Azab Construction Company",
     author_email="admin@alazab.online",
     packages=find_packages(),
     zip_safe=False,
